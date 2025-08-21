beec8664e72db9bbc89615c61fb6f5fd5f244995
Youssef alaa <youssefaeldin@gmail.com>
2025-08-21 18:27:18 +0300

    d

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,4 +12,8 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.name
+
     
