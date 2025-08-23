8e39172b9381514c0f5c9cb6ab45d805a2106bf9
Youssef alaa <youssefaeldin@gmail.com>
2025-08-23 19:59:50 +0300

    df

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,8 +14,8 @@ path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html')
 path('admin-dashboard/', admin_view, name='admin_view'),
 path('librarian-dashboard/', librarian_view, name='librarian_view'),
 path('member-dashboard/', member_view, name='member_view'),
-path('books/add/', views.add_book, name='add_book'),
-path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+path('add_book/', views.add_book, name='add_book'),
+path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
 path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
 # Optionally include this if you have a book list view
 path('books/', views.book_list, name='book_list'),
